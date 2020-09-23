bbcd817a83987fe4c6f5ff342ae1b7607a0db557
galymax <werkhowski@gmail.com>
2020-09-23 10:51:11 +0200

    fixing redirect

--- routes/index.js
+++ routes/index.js
@@ -49,7 +49,7 @@ router.post('/add', function(req, res, next) {
   const all = db.get('posts')
   .find()
   .value()
-  res.redirect('https://need2pee.de/table.html');
+  res.redirect('https://need2pee.de');
 });
 
 /* GET all */
